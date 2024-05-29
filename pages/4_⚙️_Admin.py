10aec1c810dc1411a3e0b4866d33020f40d23d1c
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-05-29 16:17:53 +0000

    adfad

--- pages/4_⚙️_Admin.py
+++ pages/4_⚙️_Admin.py
@@ -8,6 +8,7 @@ maincontainer = ps.userflow_styled_container2()
 with maincontainer:
     userinfocontainer = st.container(height=300, border=False)
     with userinfocontainer:
+        ps.get_gray_header(varText="User Basic Information")
         cols = st.columns([1, 20, 1, 20, 1])
         with cols[1]:
             st.text_input(label="First Name", value=st.session_state.firstname, disabled=True)
@@ -15,5 +16,6 @@ with maincontainer:
             st.text_input(label="Last Name", value=st.session_state.lastname, disabled=True)
     detailscontainer = st.container(height=400)
     with detailscontainer:
+        ps.get_gray_header(varText="User Detailed Information")
         tabnames = ["Chat History", "Forms", "Other"]
         tab1, tab2, tab3 = st.tabs(tabs=tabnames)
